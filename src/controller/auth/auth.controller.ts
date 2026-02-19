e4be0c52548fa4b616e89ecb8bee0df6efb4eec3
haenotfound <honey00715@gmail.com>
2026-02-19 21:50:27 +0900

    260219 21:50

--- src/controller/auth/auth.controller.ts
+++ src/controller/auth/auth.controller.ts
@@ -1,23 +1,42 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import type { Response } from 'express';
+import { ApiResponse } from 'src/common/dto/api-response.dto';
 import { LocalAuthGuard } from 'src/module/auth/guard/local-auth.guard';
+import { AuthService } from 'src/service/auth/auth.service';
 import type { AuthRequest } from 'src/type/auth.type';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
   // 로그인
-  @Post("login")
-  @UseGuards(LocalAuthGuard) 
-  // request -> guard -> local.strategy -> validate -> return -> req.user 
-  async login(@Req() req:AuthRequest){
-    // console.log("controller", req)
-    console.log("controller", req.user)
-    return {
-      message: "로그인 성공",
-      user: req.user
-    }
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  // request -> guard -> local.strategy -> validate -> return -> req.user
+  // auth login ->
+  async login(
+    @Req() req: AuthRequest,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    console.log('controller', req.user);
+
+    const { accessToken, refreshToken } = await this.authService.login(
+      req.user,
+    );
+
+    // 토큰 프론트로 보내긴하는데.. 쥐도새도 모르게 프론트는 모른다.
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      path: '/',
+      sameSite: 'lax',
+    });
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      path: '/',
+      sameSite: 'lax',
+    });
+
+    return new ApiResponse('로그인이 성공하였습니다');
   }
-
-
-
 }
--- src/service/auth/auth.service.ts
+++ src/service/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { MemberRepository } from 'src/repository/member/member.repository';
 import bcrypt from "bcrypt"
+import { JwtPayload } from 'src/type/auth.type';
+import { TokenDTO } from 'src/domain/auth/dto/auth.dto';
+import { JwtTokenService } from '../jwt/jwt.service';
 // 회원 검증과 관련된 서비스
 @Injectable()
 export class AuthService {
-  constructor(private readonly memberRepository: MemberRepository){;}
+  constructor(
+    private readonly memberRepository: MemberRepository,
+    private readonly jwtTokenService:JwtTokenService
+  ){;}
 
   // 비밀번호 해싱
   private readonly saltRounds = 10;
@@ -18,5 +24,16 @@ export class AuthService {
   async comparePassword(password:string, hashPassword:string):Promise<boolean>{
     return bcrypt.compare(password, hashPassword)
   }
+
+  // 로그인
+  async login(payload: JwtPayload):Promise<TokenDTO>{
+    const accessToken = await this.jwtTokenService.generateAccesstoken(payload)
+    const refreshToken = await this.jwtTokenService.generateAccesstoken(payload)
+
+    return {accessToken, refreshToken}
+  }
+
+  // 로그아웃
+  
 
 }
